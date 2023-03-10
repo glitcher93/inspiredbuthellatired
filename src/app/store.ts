89a268e188fc5d3a6a6028c067a75f0e0dc1087a
glitcher93 <nigeljdsouza@gmail.com>
2023-03-10 09:10:30 -0500

    set up store, imported appContentSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,1 +1,21 @@
 import {configureStore} from '@reduxjs/toolkit';
+import appContentSlice from '../features/AppContent/appContentSlice';
+
+const reducer = {
+    appContent: appContentSlice,
+}
+
+const store = configureStore({
+    reducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['appContent/setAnchor'],
+                ignoredPaths: ['appContent.anchor']
+            }
+        })
+    }
+})
+
+export default store;
