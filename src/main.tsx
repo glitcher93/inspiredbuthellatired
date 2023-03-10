04c9943362c8c69d06f1d7e6d75e0eccc479ea95
glitcher93 <nigeljdsouza@gmail.com>
2023-03-10 09:05:12 -0500

    ThemeProvider, Provider, theme, and store imported

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './app';
+import store from './app/store';
 import './index.css';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
