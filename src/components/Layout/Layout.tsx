01eefce05f103f65a390093858138af4cdf11871
glitcher93 <nigeljdsouza@gmail.com>
2023-03-13 14:53:47 -0400

    imported footer, and adjusted format of Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,23 @@
 import { makeStyles } from "@mui/styles";
 import { Theme } from "@mui/material";
 import { ILayoutProps } from "../../utils/interfaces";
+import Footer from "../Footer";
 
 
 const useStyles = makeStyles((theme: Theme) => ({
-    toolbar: theme.mixins.toolbar
+    toolbar: theme.mixins.toolbar,
+    wrapper: {
+        padding: "1rem",
+        width: "100%",
+        [theme.breakpoints.up('md')]: {
+            padding: "1rem 2.25rem"
+        },
+        [theme.breakpoints.up('lg')]: {
+            padding: "1rem 0",
+            margin: "0 auto",
+            maxWidth: "85%",
+        }
+    }
 }), {index: 1})
 
 const Layout = ({ children }: ILayoutProps) => {
@@ -12,10 +25,15 @@ const Layout = ({ children }: ILayoutProps) => {
     const classes = useStyles();
 
     return (
-        <div>
+        <>
             <div className={classes.toolbar}></div>
-            {children}
-        </div> 
+            <main
+            className={classes.wrapper}
+            >
+                {children}
+            </main>
+            <Footer />
+        </> 
     );
 }
 
