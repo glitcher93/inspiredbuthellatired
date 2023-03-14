b940da2805ebc8e3729821fa136296e23f54182e
glitcher93 <nigeljdsouza@gmail.com>
2023-03-13 21:31:47 -0400

    created ProductsGrid and Cart features, will setup later

---
+++ src/features/ProductsGrid/ProductsGrid.tsx
@@ -0,0 +1,10 @@
+import { Grid } from "@mui/material";
+
+const ProductsGrid = () => {
+    return (
+        <Grid container spacing={2}>
+        </Grid>
+    );
+}
+ 
+export default ProductsGrid;
