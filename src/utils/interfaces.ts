e47961d8deacb211a3de61ebc043b5c2b533e419
glitcher93 <nigeljdsouza@gmail.com>
2023-03-16 16:37:01 -0400

    created ICartItem

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -5,6 +5,14 @@ export interface ILayoutProps {
     children: RouteProps["children"]
 }
 
+export interface ICartItem {
+    id: number
+    title: string
+    price: number
+    image: string
+    size: string
+}
+
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
