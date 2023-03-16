30850791066176820811e35afe4f217f3badedf7
glitcher93 <nigeljdsouza@gmail.com>
2023-03-16 16:36:05 -0400

    imported functions from redux-persist, created persistor for cartSlice and added it to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit';
 import appContentSlice from '../features/AppContent/appContentSlice';
+import cartSlice from '../features/Cart/cartSlice';
+import storage from 'redux-persist/lib/storage';
+import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from 'redux-persist';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartSlice);
 
 const reducer = {
     appContent: appContentSlice,
+    cart: persistedReducer
 }
 
 const store = configureStore({
@@ -11,11 +22,13 @@ const store = configureStore({
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware({
             serializableCheck: {
-                ignoredActions: ['appContent/setAnchor'],
+                ignoredActions: ['appContent/setAnchor', FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
                 ignoredPaths: ['appContent.anchorEl']
             }
         })
     }
 })
+
+export const persistor = persistStore(store);
 
 export default store;
