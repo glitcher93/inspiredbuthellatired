3474532de4167db5f800dbe836b59c176d15d47e
glitcher93 <nigeljdsouza@gmail.com>
2023-03-17 11:17:36 -0400

    set up functionality to add/remove items from cart

--- src/features/Cart/cartSlice.ts
+++ src/features/Cart/cartSlice.ts
@@ -1,1 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { ICartItem, RootState } from "../../utils/interfaces";
+
+const initialState = {
+    cart: [] as ICartItem[]
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            if (state.cart.find(item => item.id === action.payload.id)) {
+                alert('This item is already in your cart');
+                return;
+            } else {
+                state.cart.push(action.payload);
+            }
+        },
+        removeFromCart: (state, action) => {
+            const updatedCart = state.cart.filter(item => item.id !== action.payload.id);
+            state.cart = updatedCart;
+        }
+    }
+})
+
+export const selectCart = (state: RootState) => state.cart.cart;
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
