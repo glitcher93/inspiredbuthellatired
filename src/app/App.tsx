cbb9cc52248a25f0369d90dc7fed985cca86d778
glitcher93 <nigeljdsouza@gmail.com>
2023-04-04 18:18:20 -0400

    imported AdminLogin and ProtectedRoute

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from '../components/ProtectedRoute';
+import AdminLogin from '../features/AdminLogin';
 import AppContent from '../features/AppContent';
 
 function App() {
 
   return (
     <BrowserRouter>
-      <AppContent />
+      <Routes>
+        <Route path='/*' element={<AppContent />} />
+        <Route path='/login' element={<AdminLogin />} />
+        <Route element={<ProtectedRoute />}>
+
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
