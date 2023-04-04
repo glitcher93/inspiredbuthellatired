c759b9f204a7e11d46eac3fce1f39eb32db3e499
glitcher93 <nigeljdsouza@gmail.com>
2023-04-04 18:17:50 -0400

    created ProtectedRoute component

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { selectLoginPending } from "../../features/AdminLogin/adminLoginSlice";
+
+const ProtectedRoute = () => {
+    const token = localStorage.getItem('token');
+
+    const loginPending = useSelector(selectLoginPending);
+
+    if (loginPending) {
+        return null;
+    }
+
+    return token ? <Outlet /> : <Navigate to='/' />
+}
+
+export default ProtectedRoute;
