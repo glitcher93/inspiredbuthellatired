94d0aa1cbf7981dc6c324dae39cba3bea57018e3
glitcher93 <nigeljdsouza@gmail.com>
2023-04-10 15:51:33 -0400

    added AdminContent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProtectedRoute from '../components/ProtectedRoute';
 import AdminLogin from '../features/AdminLogin';
 import AppContent from '../features/AppContent';
+import AdminContent from '../features/AdminContent';
 
 function App() {
 
@@ -9,9 +10,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/*' element={<AppContent />} />
-        <Route path='/login' element={<AdminLogin />} />
+        <Route path='login' element={<AdminLogin />} />
         <Route element={<ProtectedRoute />}>
-
+          <Route path='admin/*' element={<AdminContent />}/>
         </Route>
       </Routes>
     </BrowserRouter>
