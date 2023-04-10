5ffb06e6ac2685167509d29a3a6ee094b9793803
glitcher93 <nigeljdsouza@gmail.com>
2023-04-10 15:13:30 -0400

    created Dashboard component

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,56 @@
+import { Typography } from "@mui/material";
+import Orders from "../../features/Orders/Orders";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "../../utils/interfaces";
+import { useEffect } from "react";
+import { clearOrders, getRecentOrders, selectOrder, selectOrders } from "../../features/Orders/ordersSlice";
+import OrderModal from "../OrderModal/OrderModal";
+
+const Dashboard = () => {
+
+    const orders = useSelector(selectOrders);
+    const order = useSelector(selectOrder);
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const token = localStorage.getItem("token")?.split(' ')[1]!;
+
+    useEffect(() => {
+        dispatch(getRecentOrders({token}))
+
+        return () => {
+            dispatch(clearOrders());
+        }
+    }, [dispatch])
+
+    return (
+        <section>
+            <Typography
+            variant="h1"
+            sx={(theme) => ({
+                [theme.breakpoints.up('md')]: {
+                    fontSize: theme.typography.pxToRem(56)
+                }
+            })}
+            >
+                Hi, Alyssa!
+            </Typography>
+            <Typography
+            variant="h2"
+            sx={(theme) => ({
+                fontSize: theme.typography.pxToRem(26),
+                margin: `${theme.typography.pxToRem(16)} 0`,
+                [theme.breakpoints.up('md')]: {
+                    fonstSize: theme.typography.pxToRem(36)
+                }
+            })}
+            >
+                Welcome to your dashboard!
+            </Typography>
+            <Orders orders={orders} />
+            <OrderModal order={order} />
+        </section>
+    );
+}
+ 
+export default Dashboard;
