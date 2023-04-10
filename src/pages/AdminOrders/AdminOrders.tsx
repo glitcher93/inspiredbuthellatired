b75e8d102beb167c625f4fea57e1471cebe1644d
glitcher93 <nigeljdsouza@gmail.com>
2023-04-10 15:47:55 -0400

    created AdminOrders component

---
+++ src/pages/AdminOrders/AdminOrders.tsx
@@ -0,0 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getAllOrders, selectOrder, selectOrders } from "../../features/Orders/ordersSlice";
+import { useEffect } from "react";
+import { AppDispatch } from "../../utils/interfaces";
+import { Typography } from "@mui/material";
+import Orders from "../../features/Orders";
+import OrderModal from "../../components/OrderModal/OrderModal";
+
+const AdminOrders = () => {
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const orders = useSelector(selectOrders);
+    const order = useSelector(selectOrder);
+
+    const token = localStorage.getItem('token')?.split(' ')[1]!;
+    
+    useEffect(() => {
+        dispatch(getAllOrders({token}))
+    }, [dispatch]);
+
+    return (
+        <>
+            <section>
+                <Typography
+                variant="h1"
+                sx={(theme) => ({
+                    marginBottom: theme.typography.pxToRem(16),
+                    [theme.breakpoints.up('md')]: {
+                        fontSize: theme.typography.pxToRem(56)
+                    }
+                })}
+                >
+                    Orders
+                </Typography>
+                <Orders orders={orders} />
+                <OrderModal order={order} />
+            </section>
+        </>
+    );
+}
+ 
+export default AdminOrders;
