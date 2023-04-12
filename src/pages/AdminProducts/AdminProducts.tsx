257c9d3aa4817b9a0738f50b9c5065c64940d2ed
glitcher93 <nigeljdsouza@gmail.com>
2023-04-12 16:35:32 -0400

    imported AdminProductsGrid

--- src/pages/AdminProducts/AdminProducts.tsx
+++ src/pages/AdminProducts/AdminProducts.tsx
@@ -1,7 +1,24 @@
+import { Typography } from "@mui/material";
+import AdminProductsGrid from "../../features/AdminProductsGrid";
+
 const AdminProducts = () => {
     return (
-        <>
-        </>
+        <section>
+            <Typography
+                variant="h1"
+                sx={(theme) => ({
+                    marginBottom: theme.typography.pxToRem(16),
+                    [theme.breakpoints.up('md')]: {
+                        fontSize: theme.typography.pxToRem(56)
+                    }
+                })}
+                >
+                    Products
+                </Typography>
+                <AdminProductsGrid>
+                    
+                </AdminProductsGrid>
+        </section>
     );
 }
  
