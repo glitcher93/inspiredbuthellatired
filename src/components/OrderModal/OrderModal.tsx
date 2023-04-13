02d1462a6d3bcefb2206d43cac70c2700ca8372d
glitcher93 <nigeljdsouza@gmail.com>
2023-04-13 17:19:59 -0400

    migrated tracking and delete modal to their own components

--- src/components/OrderModal/OrderModal.tsx
+++ src/components/OrderModal/OrderModal.tsx
@@ -1,13 +1,11 @@
-import { Box, Button, Fade, IconButton, Modal, TextField, Theme, Typography } from "@mui/material";
+import { Box, Button, Fade, IconButton, Modal, Theme, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { addTrackingNumber, changeTrackingField, clearTrackingField, closeModal, deleteOrder, getAllOrders, openModal, selectDeleteModalStatus, selectModalStatus, selectTrackingModalStatus, selectTrackingNumber, toggleError } from "../../features/Orders/ordersSlice";
+import { closeModal, openModal, selectModalStatus } from "../../features/Orders/ordersSlice";
 import { AppDispatch, IOrder } from "../../utils/interfaces";
-import { Add, Cancel, Close, Delete } from "@mui/icons-material";
+import { Add, Close, Delete } from "@mui/icons-material";
 import { makeStyles } from "@mui/styles";
 import SummaryItem from "../SummaryItem";
 import clsx from "clsx";
-import { ChangeEvent, FormEvent } from "react";
-import { toast } from "react-toastify";
 
 const useStyles = makeStyles((theme: Theme) => ({
     headingContainer: {
@@ -45,11 +43,6 @@ const useStyles = makeStyles((theme: Theme) => ({
         alignItems: "center",
         alignSelf: "flex-end",
     },
-    form: {
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: theme.typography.pxToRem(50)
-    },
     buttonContainer: {
         display: 'flex',
         alignItems: 'center',
@@ -61,11 +54,6 @@ const useStyles = makeStyles((theme: Theme) => ({
         [theme.breakpoints.up('md')]: {
             display: "flex"
         }
-    },
-    deleteButtons: {
-        display: 'flex',
-        alignSelf: 'center',
-        margin: `${theme.typography.pxToRem(36)} 0`
     }
 }))
 
@@ -76,11 +64,6 @@ const OrderModal = ({ order }: {order: IOrder}) => {
     const classes = useStyles();
 
     const modalStatus = useSelector(selectModalStatus);
-    const trackingModalStatus = useSelector(selectTrackingModalStatus);
-    const trackingNumber = useSelector(selectTrackingNumber);
-    const deleteModalStatus = useSelector(selectDeleteModalStatus);
-
-    const token = localStorage.getItem("token")?.split(' ')[1]!;
 
     const handleOpen = (modal: string) => {
         dispatch(openModal({modal}))
@@ -88,44 +71,6 @@ const OrderModal = ({ order }: {order: IOrder}) => {
 
     const handleClose = (modal: string) => {
         dispatch(closeModal({modal}));
-        dispatch(clearTrackingField());
-    }
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-
-        dispatch(changeTrackingField(value))
-    }
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        if (!trackingNumber) {
-            dispatch(toggleError())
-            return;
-        }
-
-        dispatch(addTrackingNumber({token, id: order.id, trackingNumber}))
-            .then(() => {
-                handleClose("tracking");
-                toast.success("Tracking Number Added!");
-                dispatch(getAllOrders({token}));
-            })
-            .catch((err) => {
-                toast.error("Something went wrong!")
-            })
-    }
-
-    const handleDelete = () => {
-        dispatch(deleteOrder({token, id: order.id}))
-            .then(() => {
-                handleClose("delete");
-                toast.success("Order Successfully Deleted!");
-                dispatch(getAllOrders({token}))
-            })
-            .catch(() => {
-                toast.error("Order is not fulilled and can't be deleted")
-            })
     }
 
     return (
@@ -487,171 +432,6 @@ const OrderModal = ({ order }: {order: IOrder}) => {
                                 </div>
                             </div>
                         </div>
-                        <Modal
-                        open={trackingModalStatus}
-                        onClose={() => handleClose('tracking')}
-                        >
-                            <Box
-                            sx={(theme) => ({
-                                position: "absolute" as 'absolute',
-                                top: "50%",
-                                left: "50%",
-                                transform: 'translate(-50%, -50%)',
-                                backgroundColor: "#FFF",
-                                width: '85%',
-                                height: theme.typography.pxToRem(250),
-                                display: "flex",
-                                flexDirection: "column",
-                                padding: theme.typography.pxToRem(16),
-                                borderRadius: theme.typography.pxToRem(4),
-                                outline: "none",
-                                maxWidth: `492px`
-                            })}
-                            >
-                                <Close 
-                                sx={(theme) => ({
-                                    alignSelf: 'flex-end',
-                                    cursor: 'pointer'
-                                })}
-                                onClick={() => handleClose("tracking")}
-                                />
-                                <Typography
-                                variant='h4'
-                                sx={(theme) => ({
-                                    fontFamily: 'Nunito Sans',
-                                    fontSize: theme.typography.pxToRem(18)
-                                })}
-                                >
-                                    Enter Tracking Number
-                                </Typography>
-                                <form
-                                method="PATCH"
-                                className={classes.form}
-                                onSubmit={handleSubmit}
-                                >
-                                    <TextField 
-                                    label="Tracking Number"
-                                    name="trackingNumber"
-                                    value={trackingNumber}
-                                    onChange={handleChange}
-                                    sx={(theme) => ({
-                                        marginRight: theme.typography.pxToRem(16),
-                                        width: '60%'
-                                    })}
-                                    />
-                                    <Button
-                                    variant="contained"
-                                    sx={(theme) => ({
-                                        height: '100%'
-                                    })}
-                                    >
-                                        Submit
-                                    </Button>
-                                </form>
-                            </Box>
-                        </Modal>
-                        <Modal
-                        open={deleteModalStatus}
-                        onClose={() => handleClose("delete")}
-                        >
-                            <Box
-                            sx={(theme) => ({
-                                position: "absolute" as 'absolute',
-                                top: "50%",
-                                left: "50%",
-                                transform: 'translate(-50%, -50%)',
-                                backgroundColor: "#FFF",
-                                width: '85%',
-                                height: theme.typography.pxToRem(250),
-                                display: "flex",
-                                flexDirection: "column",
-                                padding: theme.typography.pxToRem(16),
-                                borderRadius: theme.typography.pxToRem(4),
-                                outline: "none",
-                                maxWidth: `492px`
-                            })}
-                            >
-                                <Close 
-                                sx={(theme) => ({
-                                    alignSelf: 'flex-end',
-                                    cursor: 'pointer'
-                                })}
-                                onClick={() => handleClose("delete")}
-                                />
-                                <Typography
-                                variant='h4'
-                                sx={(theme) => ({
-                                    fontFamily: 'Nunito Sans',
-                                    fontSize: theme.typography.pxToRem(18)
-                                })}
-                                >
-                                    Delete Order?
-                                </Typography>
-                                <Typography
-                                sx={(theme) => ({
-                                    marginTop: theme.typography.pxToRem(24),
-                                    textAlign: 'center'
-                                })}
-                                >
-                                    Are you sure you want to delete this order? (This action cannot be undone)
-                                </Typography>
-                                <div
-                                className={classes.deleteButtons}
-                                >
-                                    <Button
-                                    variant="contained"
-                                    sx={(theme) => ({
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        backgroundColor: '#DD0000',
-                                        transition: 'background-color 0.3s',
-                                        marginRight: theme.typography.pxToRem(8),
-                                        '&:hover': {
-                                            backgroundColor: '#BB0000'
-                                        }
-                                    })}
-                                    onClick={() => handleClose("delete")}
-                                    >
-                                        <Cancel 
-                                        sx={(theme) => ({
-                                            fontSize: `1rem`,
-                                            marginRight: theme.typography.pxToRem(4)
-                                        })}
-                                        />
-                                        <Typography
-                                        sx={(theme) => ({
-                                            fontSize: `1rem`
-                                        })}
-                                        >
-                                            Cancel
-                                        </Typography>
-                                    </Button>
-                                    <Button
-                                    variant="contained"
-                                    sx={(theme) => ({
-                                        marginRight: theme.typography.pxToRem(8),
-                                        display: 'flex',
-                                        alignItems: 'center'
-                                    })}
-                                    onClick={handleDelete}
-                                    >
-                                        <Delete 
-                                        sx={(theme) => ({
-                                            fontSize: `1rem`,
-                                            marginRight: theme.typography.pxToRem(4)
-                                        })}
-                                        />
-                                        <Typography
-                                        sx={(theme) => ({
-                                            fontSize: `1rem`
-                                        })}
-                                        >
-                                            Delete
-                                        </Typography>
-                                    </Button>
-                                </div>
-                            </Box>
-                        </Modal>
                     </Box>
                 </Fade>
             </Modal>
